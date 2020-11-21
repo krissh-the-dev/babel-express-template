231b60761a5ebd09853c43cfa4fa08bd51d1b4f6
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-11-21 15:25:16 +0000

    added requesting logging using morgan

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const server = express();
+
+// Request logging: refer - https://www.npmjs.com/package/morgan
+server.use(morgan('tiny'));
 
 server.get('/', (req, res) => {
   // write code
