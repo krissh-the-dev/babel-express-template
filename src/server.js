c06a452ccd1c9e2ce929f2c492d5eb3d8efa89b5
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-11-23 23:17:22 +0530

    added advanced logging options with js-logger & dotenv

--- src/server.js
+++ src/server.js
@@ -1,17 +1,22 @@
 import express from 'express';
+import Logger from 'js-logger';
+import chalk from 'chalk';
+import 'dotenv/config';
 
 import { requestLogger } from './middlewares';
 
+const PORT = process.env.PORT || 5000;
+Logger.useDefaults();
+Logger.setLevel(process.env.NODE_ENV === 'production' ? Logger.ERROR : Logger.INFO);
 const server = express();
 
-// Request logging: refer - https://www.npmjs.com/package/morgan
 server.use(requestLogger);
 
-server.get('/', async (req, res) => {
-  /* Write your code */
+server.get('/', (req, res) => {
   res.sendStatus(200);
 });
 
-server.listen(5000, () => {
-  console.log('Server started');
+server.listen(PORT, () => {
+  Logger.info(`Server started at port ${chalk.magenta(PORT)}`);
+  Logger.info(`Listening for requests at: ${chalk.cyan(`http://127.0.0.1:${PORT}`)}`);
 });
