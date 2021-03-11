a066d2ed035b38213bc459b8288a8178c9462c89
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-03-11 18:29:24 +0530

    updated prettier configuration

--- src/middlewares/requestLogger.js
+++ src/middlewares/requestLogger.js
@@ -2,65 +2,65 @@ import morgan from 'morgan';
 import chalk from 'chalk';
 
 export default morgan((tokens, req, res) => {
-  let status = tokens.status(req, res);
-  let method = tokens.method(req, res);
-  let url = tokens.url(req, res);
-  let length = tokens.res(req, res, 'content-length');
-  let resTime = tokens['response-time'](req, res);
-  let time = new Date(tokens.date(req, res)).toLocaleTimeString();
-  let date = new Date(tokens.date(req, res)).toLocaleDateString('en-GB');
+	let status = tokens.status(req, res);
+	let method = tokens.method(req, res);
+	let url = tokens.url(req, res);
+	let length = tokens.res(req, res, 'content-length');
+	let resTime = tokens['response-time'](req, res);
+	let time = new Date(tokens.date(req, res)).toLocaleTimeString();
+	let date = new Date(tokens.date(req, res)).toLocaleDateString('en-GB');
 
-  if (status < 200) {
-    status = chalk.gray.bold(status);
-  } else if (status < 300) {
-    status = chalk.green.bold(status);
-  } else if (status < 400) {
-    status = chalk.cyan.bold(status);
-  } else if (status < 500) {
-    status = chalk.yellow.bold(status);
-  } else {
-    status = chalk.red.bold(status);
-  }
+	if (status < 200) {
+		status = chalk.gray.bold(status);
+	} else if (status < 300) {
+		status = chalk.green.bold(status);
+	} else if (status < 400) {
+		status = chalk.cyan.bold(status);
+	} else if (status < 500) {
+		status = chalk.yellow.bold(status);
+	} else {
+		status = chalk.red.bold(status);
+	}
 
-  switch (method) {
-    case 'GET':
-      method = chalk.blue(method);
-      break;
+	switch (method) {
+		case 'GET':
+			method = chalk.blue(method);
+			break;
 
-    case 'POST':
-    case 'PUT':
-      method = chalk.magenta(method);
-      break;
+		case 'POST':
+		case 'PUT':
+			method = chalk.magenta(method);
+			break;
 
-    case 'PATCH':
-      method = chalk.yellow(method);
-      break;
+		case 'PATCH':
+			method = chalk.yellow(method);
+			break;
 
-    case 'DELETE':
-      method = chalk.red(method);
-      break;
+		case 'DELETE':
+			method = chalk.red(method);
+			break;
 
-    default:
-      break;
-  }
+		default:
+			break;
+	}
 
-  if (length >= 600 && length < 3000) {
-    length = chalk.yellow(length + 'B');
-  } else if (length >= 3000) {
-    length = chalk.red(length + 'B');
-  } else if (!length) {
-    length = '0B';
-  } else {
-    length += 'B';
-  }
+	if (length >= 600 && length < 3000) {
+		length = chalk.yellow(length + 'B');
+	} else if (length >= 3000) {
+		length = chalk.red(length + 'B');
+	} else if (!length) {
+		length = '0B';
+	} else {
+		length += 'B';
+	}
 
-  if (resTime >= 500 && resTime < 1000) {
-    resTime = chalk.yellow(resTime + 'ms');
-  } else if (resTime >= 1000) {
-    resTime = chalk.red(resTime + 'ms');
-  } else {
-    resTime += 'ms';
-  }
+	if (resTime >= 500 && resTime < 1000) {
+		resTime = chalk.yellow(resTime + 'ms');
+	} else if (resTime >= 1000) {
+		resTime = chalk.red(resTime + 'ms');
+	} else {
+		resTime += 'ms';
+	}
 
-  return `[${date} ${time}] ${status} | ${method} ${url} ${length} | ${resTime}`;
+	return `[${date} ${time}] ${status} | ${method} ${url} ${length} | ${resTime}`;
 });
--- src/server.js
+++ src/server.js
@@ -7,20 +7,22 @@ import { requestLogger } from './middlewares';
 
 const PORT = process.env.PORT || 5000;
 Logger.useDefaults({
-  defaultLevel: process.env.NODE_ENV === 'production' ? Logger.ERROR : Logger.INFO,
-  formatter: messages => {
-    messages.unshift(`[${new Date().toLocaleDateString('en-GB')} ${new Date().toLocaleTimeString()}]`);
-  }
+	defaultLevel: process.env.NODE_ENV === 'production' ? Logger.ERROR : Logger.INFO,
+	formatter: messages => {
+		messages.unshift(
+			`[${new Date().toLocaleDateString('en-GB')} ${new Date().toLocaleTimeString()}]`
+		);
+	}
 });
 const server = express();
 
 server.use(requestLogger);
 
 server.get('*', (_req, res) => {
-  res.sendStatus(200);
+	res.sendStatus(200);
 });
 
 server.listen(PORT, () => {
-  Logger.info(`Server started at port ${chalk.magenta(PORT)}`);
-  Logger.info(`Listening for requests at: ${chalk.cyan(`http://127.0.0.1:${PORT}`)}`);
+	Logger.info(`Server started at port ${chalk.magenta(PORT)}`);
+	Logger.info(`Listening for requests at: ${chalk.cyan(`http://127.0.0.1:${PORT}`)}`);
 });
