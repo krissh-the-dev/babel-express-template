effe0ca20c090367994185d9517ed3d19d4adcfa
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-04-11 21:13:10 +0530

    made logRequests controllable from env

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -1,4 +1,13 @@
+/**
+ * These values will be loaded from '.env' file
+ * refer docs: https://github.com/lorenwest/node-config/wiki/Environment-Variables
+ * these values will be loaded from other configs if env is not set
+ *
+ * logRequests can be exposed from env to make it easier to troubleshoot in production
+ */
+
 module.exports = {
 	host: 'HOST',
-	port: 'PORT'
+	port: 'PORT',
+	logRequests: 'VERBOSE_REQ_LOGGING'
 };
