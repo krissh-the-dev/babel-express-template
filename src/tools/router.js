e25c2aad4ece7ae975751332d9f148a82d77cbfe
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-04-15 08:27:34 +0530

    added async error handler

--- src/tools/router.js
+++ src/tools/router.js
@@ -1,5 +1,14 @@
+import { StatusCodes } from 'http-status-codes';
 import { PingRouter } from 'routers';
 
 export default function registerRouters(app) {
 	app.use('/ping', PingRouter);
+
+	app.use((err, _req, res, next) => {
+		res
+			.status(StatusCodes.INTERNAL_SERVER_ERROR)
+			.send('Something went wrong from our side. Please try again after some time.');
+
+		next(err);
+	});
 }
