fd37df4583e78201e57c1f7956a1db3aec5a6892
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-05-14 08:35:04 +0530

    changed production log level to info & log more details on startup

--- src/tools/listener.js
+++ src/tools/listener.js
@@ -1,10 +1,18 @@
 import chalk from 'chalk';
 import logger from '@tools/logging';
+import config from 'config';
+import { name as appName, version } from 'package.json';
 
 export default function registerListener(app, PORT, HOST) {
+	const environment = config.util.getEnv('NODE_ENV');
 	const server = app.listen(PORT, HOST, () => {
 		const { address, port } = server.address();
-		logger.info(`Server started at port ${chalk.magenta(port)}`);
+		logger.info(
+			`Started ${appName} v${version} ${chalk.yellow(environment)} server at port ${chalk.magenta(
+				port
+			)}`
+		);
 		logger.info(`Listening for requests at ${chalk.cyan(address + ':' + port)}`);
+		logger.info(`Logging level set to: ${chalk.blue(logger.level)}`);
 	});
 }
