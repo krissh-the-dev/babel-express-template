753db689dd673b6f76bde629666a772966745ae4
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-07-14 09:19:09 +0530

    added warning comments for clustering switch

--- config/default.js
+++ config/default.js
@@ -4,3 +4,11 @@ module.exports = {
 	serveDocument: true,
 	enableClustering: false
 };
+
+/*
+	enabling clustering can negatively impact the performance for non-cpu intensive apps,
+	enable only if you need more cpus to handle requests (will drastically improve performance for cpu intensive apps)
+	also, clustering increases complexity and disables state sharing
+
+	read more about performance analysis here: https://blog.appsignal.com/2021/02/03/improving-node-application-performance-with-clustering.html
+*/
